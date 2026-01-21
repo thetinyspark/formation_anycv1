6402a2772639b717cfd61920caa92b7c62ebfb21
BlackB34rd <contact@the-tiny-spark.com>
2026-01-21 11:57:41 +0100

    add correction TP n°2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,24 @@
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './components/catalog/catalog.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routeConfig: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home'
+  },
   {
     path: 'catalog',
     component: CatalogComponent,
     title: 'Catalog'
-  }
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'Cart'
+  },
 ];
 
 export default routeConfig;
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet,  } from '@angular/router';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, NavBarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
