1e372bf6d4be183543159455c16dfe3114e225a1
BlackB34rd <contact@the-tiny-spark.com>
2026-01-23 15:18:21 +0100

    add service test example + providers

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
+import { firstValueFrom, Observable, of } from 'rxjs';
+import { PRODUCTS } from '../models/product';
+import { HttpClient } from '@angular/common/http';
 
-describe('ProductService', () => {
+fdescribe('ProductService', () => {
   let service: ProductService;
 
+  class FakeHttpClient{
+    get<T>(url:string):Observable<any>{
+      return of(PRODUCTS);
+    }
+  }
+
+  const http = new FakeHttpClient();
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: http }
+      ]
+    });
     service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return products', async () => {
+    const results = await firstValueFrom( service.getProducts() );
+    expect(results).toEqual(PRODUCTS);
+  });
+
+  it('should refresh products and update product signal', async () => {
+    await service.refresh();
+    expect(service.products()).toEqual(PRODUCTS);
+  });
+
+  it('should add a product to the cart and update cart signal', async () => {
+    // await service.refresh();
+    // expect(service.products()).toEqual(PRODUCTS);
   });
 });
