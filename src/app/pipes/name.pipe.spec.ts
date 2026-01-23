057c375dab3480d62ad2f7da5a73c3cd7afce427
BlackB34rd <contact@the-tiny-spark.com>
2026-01-23 15:05:51 +0100

    correction tp N°16

--- src/app/pipes/name.pipe.spec.ts
+++ src/app/pipes/name.pipe.spec.ts
@@ -1,8 +1,27 @@
+import { PRODUCTS } from '../models/product';
 import { NamePipe } from './name.pipe';
 
-describe('NamePipe', () => {
+fdescribe('NamePipe', () => {
   it('create an instance', () => {
     const pipe = new NamePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should not have resulting products', () => {
+    const pipe = new NamePipe();
+    const products = PRODUCTS; 
+
+    const results = pipe.transform(products, "---"); 
+
+    expect(results.length).toEqual(0);
+  });
+  
+  it('should have products[0] at least', () => {
+    const pipe = new NamePipe();
+    const products = PRODUCTS; 
+
+    const results = pipe.transform(products, products[0].name.substring(0,2)); 
+
+    expect(results).toContain(products[0]);
   });
 });
